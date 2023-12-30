b994c0326248dc8718d88af7b0ad31d0d7fd459e
JNGS <143780288+jiffgals@users.noreply.github.com>
2023-12-30 14:12:35 +0800

    Delete ims directory

--- ims/partials/app-topnav.php
+++
@@ -1,4 +0,0 @@
-<div class="dashboard_topNav" id="dashboard_topNav">
-    <a href="" id="toggleBtn"><i class="fa fa-navicon"></i></a>
-    <a href="logout.php" id="logoutBtn"><i class="fa fa-power-off"></i> Log-out</a>
-</div>
