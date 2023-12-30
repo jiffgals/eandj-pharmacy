aec1f1a7ea9159ab65f09ed4aeebbef969b57cd6
JNGS <143780288+jiffgals@users.noreply.github.com>
2023-12-30 20:37:49 +0800

    Update users-add.html

--- users-add.php
+++ users-add.php
@@ -6,7 +6,7 @@
     $_SESSION['redirect_to'] = 'users-add.php';
 
     $user = $_SESSION['$user'];
-    $users = include('database/show-users.php');
+    $users = include('database/show-users.html');
 ?>
 
 <DOCTYPE html>
